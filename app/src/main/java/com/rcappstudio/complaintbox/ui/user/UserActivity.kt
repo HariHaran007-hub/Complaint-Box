b6c2db040bc39da04e0d161c54d36ca28fdd0bb1
HARI HARAN R C <81560319+HariHaran007-hub@users.noreply.github.com>
2023-01-30 20:40:53 +0530

    First commit

--- app/src/main/java/com/rcappstudio/complaintbox/ui/user/UserActivity.kt
+++ app/src/main/java/com/rcappstudio/complaintbox/ui/user/UserActivity.kt
@@ -2,8 +2,14 @@ package com.rcappstudio.complaintbox.ui.user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.navigation.NavigationBarView
 import com.rcappstudio.complaintbox.R
+import com.rcappstudio.complaintbox.databinding.ActivityUserBinding
 import com.rcappstudio.complaintbox.ui.user.viewmodel.UserViewModel
 import com.rcappstudio.complaintbox.ui.user.viewmodel.UserViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,10 +24,57 @@ class UserActivity : AppCompatActivity() {
 
     private lateinit var viewModel: UserViewModel
 
+    private lateinit var binding: ActivityUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
-
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         viewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation(){
+        binding.userBottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+
+                R.id.all->{
+                    switchToFragment(R.id.userFragment1)
+                }
+                R.id.pending->{
+                    switchToFragment(R.id.userFragment2)
+                }
+                R.id.completed->{
+                    switchToFragment(R.id.userFragment3)
+                }
+            }
+            true
+        }
+    }
+
+
+    private fun getNavController(): NavController {
+        return (supportFragmentManager.findFragmentById(R.id.userFragmentContainerView) as NavHostFragment).navController
+    }
+
+    private fun switchToFragment(destinationId: Int) {
+        if (isFragmentInBackStack(destinationId)) {
+            getNavController().popBackStack(destinationId, false)
+        } else {
+            getNavController().navigate(destinationId)
+        }
+    }
+
+    private fun isFragmentInBackStack(destinationId: Int) =
+        try {
+            getNavController().getBackStackEntry(destinationId)
+            true
+        } catch (e: Exception) {
+            false
+        }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        binding.userBottomNavigationView.selectedItemId = R.id.all
     }
 }
